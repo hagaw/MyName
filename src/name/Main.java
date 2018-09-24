2dc794ebf7cd3d21b941b106c8943859f84329d7
ryanweaver718 <ryan.weaver718@gmail.com>
2018-09-24 16:13:42 -0400

    name

---
+++ src/name/Main.java
@@ -0,0 +1,15 @@
+package name;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner myName=new Scanner(System.in);
+
+        System.out.println("inter your name");
+            String myname=myName.nextLine();
+              System.out.println("My Name is " + myname);
+
+	// write your code here
+    }
+}
